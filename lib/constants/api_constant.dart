5ac664a8d4ac7897920b08019e10863e9540ac6f
Luthfi1010 <lluthfii348@gmail.com>
2026-06-27 19:15:00 +0800

    Sesuaikan dengan API Devicenya

--- lib/constants/api_constant.dart
+++ lib/constants/api_constant.dart
@@ -1,5 +1,5 @@
 class ApiConstant {
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
+  static const String baseUrl = 'http://192.168.100.4:8000/api';
 
   static const String login    = '$baseUrl/login';
   static const String register = '$baseUrl/register';
